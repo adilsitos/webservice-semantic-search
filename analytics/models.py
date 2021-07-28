ec922a5ec3be2cd63d029c764509f5c2f9d38f2a
Adilson <adilsonfrankneia@gmail.com>
2021-07-27 21:17:40 -0300

    adding null values in recommendation

--- analytics/models.py
+++ analytics/models.py
@@ -8,8 +8,8 @@ class Recommendation(models.Model):
         verbose_name= "Recommendation"
         verbose_name_plural= "Recommendation"
 
-    users_response = models.IntegerField(default=0, verbose_name="Response from users")
-    searched_query = models.CharField(max_length=500, verbose_name="Query from the user")
-    first = models.CharField(max_length=500, verbose_name="Highest recommendation")
-    second = models.CharField(max_length=500, verbose_name="Second Highest recommendation")
-    third = models.CharField(max_length=500, verbose_name="Third Highest recommendation")
+    users_response = models.IntegerField(default=0, verbose_name="Response from users", null=False)
+    searched_query = models.CharField(max_length=500, verbose_name="Query from the user", null=True)
+    first = models.CharField(max_length=500, verbose_name="Highest recommendation", null=True)
+    second = models.CharField(max_length=500, verbose_name="Second Highest recommendation", null=True)
+    third = models.CharField(max_length=500, verbose_name="Third Highest recommendation", null=True)
