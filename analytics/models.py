c24c9df2024104ff5bf03d6850f157128c803bda
Adilson <adilsonfrankneia@gmail.com>
2021-07-29 11:09:17 -0300

    fixing user response in admin

--- analytics/models.py
+++ analytics/models.py
@@ -8,7 +8,12 @@ class Recommendation(models.Model):
         verbose_name= "Recommendation"
         verbose_name_plural= "Recommendation"
 
-    user_response = models.BooleanField(default=False, verbose_name="Response from users", null=False)
+    USER_OPTIONS = (
+        (0, 'No'),
+        (1, 'Yes')
+    )
+
+    user_response = models.IntegerField(choices=USER_OPTIONS, verbose_name="Response from users", null=False)
     searched_query = models.CharField(max_length=500, verbose_name="Query from the user", null=True)
     first = models.CharField(max_length=500, verbose_name="Highest recommendation", null=True)
     first_score = models.FloatField(null=True, verbose_name="Score of the highest recommendation")
